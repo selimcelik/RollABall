7cb5173b2e6fafae632f1e0e189dd65c7c1555dc
selimcelikofficial <selimc1761@gmail.com>
2019-08-30 11:53:10 +0300

    Add files via upload

---
+++ Kodlar/TopKontrol.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopKontrol : MonoBehaviour
+{
+    Rigidbody fizik;
+    public int hiz;
+   
+    void Start()
+    {
+        fizik = GetComponent<Rigidbody>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        float yatay = Input.GetAxisRaw("Horizontal");
+        float dikey = Input.GetAxisRaw("Vertical");
+
+        Vector3 vec = new Vector3(yatay, 0, dikey);
+
+        fizik.AddForce(vec*hiz);
+        //Debug.Log("yatay= "+yatay+"    dikey= "+dikey);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "engel")
+        {
+            other.gameObject.SetActive(false);
+            
+            
+        }
+        
+        
+    }
+}
